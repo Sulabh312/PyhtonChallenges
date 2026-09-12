f1719f4c6cdcb088d1be4d74011a44d53b01b2f3
Sulabh312 <sulabhgupta312@gmail.com>
2026-09-13 00:26:07 +0530

    U2P5

---
+++ U2P5.py
@@ -0,0 +1,17 @@
+import numpy as np, matplotlib.pyplot as plt
+
+a3 = -2
+a2 = 1 - a3
+a1, a0 = 0, 0 
+def x(t):
+    return round((a0 + a1 * t + a2 * (t**2) + a3 * (t**3)), 6)
+t_val = np.round(np.arange(0, 1.1, 0.1),2)
+for t in t_val:
+    print(f"{t}\t{x(t)}")
+
+plt.plot(t_val, [x(t) for t in t_val])
+plt.xlabel("Time (t)")
+plt.ylabel("Position x(t)")
+plt.title("Cubic Trajectory")
+plt.grid(True)
+plt.show()
