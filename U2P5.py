719cea1b605c20e5b3d73663be1856058630d49c
Sulabh312 <sulabhgupta312@gmail.com>
2026-09-13 00:32:42 +0530

    U2P5

--- U2P5.py
+++ U2P5.py
@@ -3,7 +3,8 @@ import numpy as np, matplotlib.pyplot as plt
 a3 = -2
 a2 = 1 - a3
 a1, a0 = 0, 0 
-
+def x(t):
+    return round((a0 + a1 * t + a2 * (t**2) + a3 * (t**3)), 6)
 t_val = np.round(np.arange(0, 1.1, 0.1),2)
 for t in t_val:
     print(f"{t}\t{np.polyval([a3, a2, a1, a0], t)}")
